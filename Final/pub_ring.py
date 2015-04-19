2d7a6c5046f9adb1a6c7807e3c60a7e509c00650
Mike Linnen <mlinnen@protosystem.net>
2015-04-19 15:31:08 -0400

    removed the unused gpio pin

--- Final/pub_ring.py
+++ Final/pub_ring.py
@@ -4,12 +4,12 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     if GPIO.input(24) == 1:
         print("front button pressed")
+		# TODO change the 1 to the Door Number you were given with your Pi.
         publish.single("protosystem/door/1/ring", "", hostname="test.mosquitto.org")
         time.sleep(2)
 
