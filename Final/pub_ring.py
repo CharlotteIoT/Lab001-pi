4b586143977bf1caa2b8fe75f0c30319325872ca
Mike Linnen <mlinnen@protosystem.net>
2015-04-18 21:00:55 -0400

    Initial Checkin of code for the lab

---
+++ Final/pub_ring.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import paho.mqtt.publish as publish
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while True:
+    if GPIO.input(24) == 1:
+        print("front button pressed")
+        publish.single("protosystemdemo/door/1/ring", "", hostname="test.mosquitto.org")
+        time.sleep(2)
+
+GPIO.cleanup()
---
+++ Final/sub_ring.py
@@ -0,0 +1,33 @@
+import paho.mqtt.client as mqtt
+import subprocess
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+	print("Connected with result code "+str(rc))
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe("protosystemdemo/door/+/ring")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+	print(msg.topic+" "+str(msg.payload))
+	if msg.payload == "1":
+		filenamering1 = r'./doorbell-1.wav'
+		subprocess.Popen([ "/usr/bin/aplay", '-q', filenamering1 ] )
+	if msg.payload == "2":
+		filenamering2 = r'./doorbell-2.wav'
+		subprocess.Popen([ "/usr/bin/aplay", '-q', filenamering2 ] )
+	
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("test.mosquitto.org", 1883, 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
+client.loop_forever()
